c0be7baf6091290f0bc18e786f9d87b2663ed63e
Pradip7028 <pradipmalage7028@gmail.com>
2025-10-07 14:03:49 +0000

    done

---
+++ .devcontainer/day1/datatype.js
@@ -0,0 +1,8 @@
+let a="Hello"
+const b="pradip"
+var c=101;
+
+
+console.log(a)
+console.log(b)
+console.log(c)
---
+++ .devcontainer/day1/variable.js
@@ -0,0 +1,10 @@
+const ac_id = 101; //constant variable declaration
+let ac_pass = 123;
+var ac_email = "abc@gmail.com";
+ac_city = "pune";
+
+ac_pass = 12345;
+ac_email = "xyz@gmail.com";
+
+//console.log(ac_id);
+console.table([ac_id, ac_pass, ac_email, ac_city]);
