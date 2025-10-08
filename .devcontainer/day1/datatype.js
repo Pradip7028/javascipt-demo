f63718b6a46f94a30067c5e8b1f3f0c4a6bc7fa9
Pradip7028 <pradipmalage7028@gmail.com>
2025-10-08 06:28:14 +0000

    done

--- .devcontainer/day1/datatype.js
+++ .devcontainer/day1/datatype.js
@@ -1,8 +1,32 @@
-let a="Hello"
+/*let a="Hello"
 const b="pradip"
 var c=101;
-
-
 console.log(a)
 console.log(b)
 console.log(c)
+*/
+
+/*datatypes- 
+ # Primitive - String,Number,Boolean,null,undefined,symbol,BigInt
+ # Non-Primitive - Array, Function, Object
+*/
+
+let score = 100;
+let score1 = 100.4;
+const isloggedIn = false;
+const outsideTemp = null;
+let message = "Hello";
+
+const id = Symbol("123"); // Symbol is a primitive data type introduced in ES6 (ECMAScript 2015) that represents a unique and immutable identifier.
+const userid = Symbol("123");
+console.log(id == userid);
+
+let bigNumber = 1245678787n;
+//console.log(typeof bigNumber);
+
+const myfunction = function () {
+  console.log("Hello world!");
+};
+myfunction();
+
+console.log(typeof myfunction);
